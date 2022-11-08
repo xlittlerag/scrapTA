01655b041008862d1b3550c93716b655fe740753
Francis <francis@dornbusch.com>
2022-11-08 19:38:49 -0300

    NativeNotifier added and clear info message

--- lib/notify.ts
+++ lib/notify.ts
@@ -1,5 +1,7 @@
-interface INotifier {
-  notify(message: string): boolean|Promise<boolean>;
+import { Notification } from "https://deno.land/x/deno_notify@1.3.1/ts/mod.ts";
+
+export interface INotifier {
+  notify(message: string): boolean | Promise<boolean>;
 }
 
 export class PostNotifier implements INotifier {
@@ -10,18 +12,29 @@ export class PostNotifier implements INotifier {
       this.post_endpoint,
       {
         method: "POST",
-        body: message
-      }
-    )
-    console.log(response)
+        body: message,
+      },
+    );
+    console.log(response);
 
+    return true;
+  }
+}
+
+export class NativeNotifier implements INotifier {
+  notify(message: string): boolean {
+    new Notification({ linux: true, windows: true })
+      .title("Ticket Alert")
+      .body(message)
+      .timeout(60000)
+      .show();
     return true;
   }
 }
 
 export class ConsoleNotifier implements INotifier {
   notify(message: string): boolean {
-    console.log(message)
+    console.log(message);
     return true;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-#!/bin/env -S deno run --allow-net --allow-env
-import { ConsoleNotifier, PostNotifier } from "../lib/notify.ts";
+#!/bin/env -S deno run --allow-net --allow-env --allow-read --allow-ffi --unstable
+import {
+  ConsoleNotifier,
+  INotifier,
+  NativeNotifier,
+  PostNotifier,
+} from "../lib/notify.ts";
 import { checkAvailableTickets } from "../lib/scraping.ts";
 import { Query, TicketStatus } from "../lib/types.ts";
 import { citiesMap } from "../lib/utils.ts";
 import json_queries from "../queries.json" assert { type: "json" };
 
 const POST_URL = Deno.env.get("POST_URL");
-if (!(POST_URL)) console.error("No POST endpoint provided. Falling back to console logging");
-const notifier = POST_URL ? new PostNotifier(POST_URL) : new ConsoleNotifier();
+if (!(POST_URL)) {
+  console.info(
+    "[Info] No POST endpoint provided. Falling back to console logging and system notifier",
+  );
+}
+const notifiers: INotifier[] = [
+  new ConsoleNotifier(),
+  new NativeNotifier(),
+];
+POST_URL && notifiers.push(new PostNotifier(POST_URL));
 
 const queries: Query[] = json_queries;
 
-(await Promise.all(queries.map(processQuery))).every((response: boolean) => response == true)
-|| console.log("No tickets found")
+(await Promise.all(queries.map(processQuery))).every((response: boolean) =>
+  response == true
+) ||
+  console.log("No tickets found");
 
 async function processQuery(q: Query): Promise<boolean> {
   const ticketStatus: TicketStatus = await checkAvailableTickets(
@@ -20,12 +35,15 @@ async function processQuery(q: Query): Promise<boolean> {
     citiesMap.getCityCode(q.to),
     q.searchDate,
     q.wantedDays,
-  )
-  if(ticketStatus.availableTickets.length != 0) {
-    return await notifier.notify(
-      `${citiesMap.getCityName(ticketStatus.fromCity)} a ${
-        citiesMap.getCityName(ticketStatus.toCity)
-      }: ${ticketStatus.availableTickets}`,
-    )
-  } else return false
+  );
+  if (ticketStatus.availableTickets.length != 0) {
+    notifiers.map(async (notifier) =>
+      await notifier.notify(
+        `${citiesMap.getCityName(ticketStatus.fromCity)} a ${
+          citiesMap.getCityName(ticketStatus.toCity)
+        }: ${ticketStatus.availableTickets}`,
+      )
+    );
+    return true;
+  } else return false;
 }
